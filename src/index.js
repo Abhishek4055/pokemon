5caced560142df420f2bfe022b81e4cb09d6985d
Abhishek <abhishekanand.jnv@gmail.com>
2022-08-15 22:55:24 +0530

    edit in Box

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import { Provider } from 'react-redux';
-import store from './redux/store'
-import { Box } from '@mui/material';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { Box } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store} >
-  <Box sx={{background: "linear-gradient(to right, #c9ffbf, #ffafbd)"}}></Box>
-    <App />
-  <Box/>
+  <Provider store={store}>
+    <Box sx={{ background: "linear-gradient(to right, #c9ffbf, #ffafbd)" }}>
+      <App />
+    </Box>
   </Provider>
-
 );
-
-
